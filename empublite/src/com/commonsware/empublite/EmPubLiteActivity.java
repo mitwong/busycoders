d33550fd3d1df9c28a7e6b139ce9c23aa3f53529
Mitchell Wong <mitwong@umich.edu>
2014-05-26 13:26:17 -0400

    Starting to integrate ActionBarSherlock
    Added ActionBarSherlock as a library to EmPubLite.
    
    Modified imports and extends in EmPubLite to work with
    ActionBarSherlock.
    
    For menu inflation, getMenuInflater had to be changed to
    getSupportMenuInflater.

--- empublite/src/com/commonsware/empublite/EmPubLiteActivity.java
+++ empublite/src/com/commonsware/empublite/EmPubLiteActivity.java
@@ -1,10 +1,11 @@
 package com.commonsware.empublite;
 
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
 
-public class EmPubLiteActivity extends Activity {
+public class EmPubLiteActivity extends SherlockActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +17,7 @@ public class EmPubLiteActivity extends Activity {
   public boolean onCreateOptionsMenu(Menu menu) {
     // Inflate the menu; this adds items to the action bar
     // if it is present.
-    getMenuInflater().inflate(R.menu.activity_main, menu);
+    getSupportMenuInflater().inflate(R.menu.activity_main, menu);
     return true;
   }
 
